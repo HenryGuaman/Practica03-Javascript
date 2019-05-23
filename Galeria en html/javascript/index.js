b935db1b86cf085f5d97d12424e3d883cee8a576
HenryGuaman <34074520+HenryGuaman@users.noreply.github.com>
2019-05-23 14:57:37 -0500

    Galeria final

--- Galeria en html/javascript/index.js
+++ Galeria en html/javascript/index.js
@@ -1,54 +1,47 @@
+var slideIndex = 1;
+showDivs(slideIndex);
 
-var actual = 0;
-function puntos(n){
-    var ptn = document.getElementsByClassName("punto");
-    for(i = 0; i<ptn.length; i++){
-        if(ptn[i].className.includes("activo")){
-            ptn[i].className = ptn[i].className.replace("activo", "");
-            break;
-        }
-    }
-    ptn[n].className += " activo";
-}
-function mostrar(n){
-    var imagenes = document.getElementsByClassName("imagen");
-    for(i = 0; i< imagenes.length; i++){
-        if(imagenes[i].className.includes("actual")){
-            imagenes[i].className = imagenes[i].className.replace("actual", "");
-            break;
-        }
-    }
-    actual = n;
-    imagenes[n].className += " actual";
-    puntos(n);
+window.onload = function() {
+	iniciar();
 }
 
-function siguiente(){
-    actual++;
-    if(actual > 4){
-        actual = 0;
-    }
-    mostrar(actual);
-}
-function anterior(){
-    actual--;
-    if(actual < 0){
-        actual = 4;
-    }
-    mostrar(actual);
+function iniciar() {
+	slideIndex = 1;
+	showDivs(1);
+	array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	var val;
+	for(i = 0; i < 5; i++) 
+		array.splice(Math.floor(Math.random() * array.length), 1);
+
+	shuffle(array);
+
+	for(i = 0; i < 5; i++) 
+		document.getElementById(i).src = "../images/"+ array[i] +".jpg";
 }
 
-var velocidad = 2000;
-var play = setInterval("siguiente()", velocidad);
+function plusDivs(n) {
+	showDivs(slideIndex += n);
+}
 
-function playpause(){
-    var boton = document.getElementById("btn");
-    if(play == null){
-        boton.src = "../images/pause.png";
-        play = setInterval("siguiente()", velocidad);
-    } else {
-        clearInterval(play);
-        play = null;
-        boton.src = "../images/play.png";
-    }
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("Presentacion");
+	if (n == x.length)
+		document.getElementById('right').disabled = true;
+	else
+		document.getElementById('right').disabled = false;
+
+	if (n == 1) 
+		document.getElementById('left').disabled = true;
+	else
+		document.getElementById('left').disabled = false
+
+	for (i = 0; i < x.length; i++)
+		x[i].style.display = "none";
+
+	x[slideIndex-1].style.display = "block"; 
+}
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
 }
